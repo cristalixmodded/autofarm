41bc6e43c572d2072397202e055ead6c7ba88de1
cristalixmodded <120457846+cristalixmodded@users.noreply.github.com>
2022-12-28 18:58:52 +1000

    Update farm.py

--- farm.py
+++ farm.py
@@ -8,14 +8,14 @@ ores_warp = config.get('Config', 'ores_warp')
 ores_slot = config.get('Config', 'ores_slot')
 mobs_warp = config.get('Config', 'mobs_warp')
 mobs_slot = config.get('Config', 'mobs_slot')
-delay = config.get('Config', 'delay') * 60
+delay = config.get('Config', 'delay')
+delay = int(delay) * 60
 
 def farm(warp, slot, click_range, time_sleep):
     for _ in range(2):
         pyautogui.press('t')
-        pyautogui.typewrite("/warp " + warp, interval=0.25)
+        pyautogui.typewrite(warp, interval=0.25)
         pyautogui.press('enter')
-        time.sleep(4)
    
     pyautogui.press(slot)
     for _ in range(click_range):
@@ -25,9 +25,11 @@ def farm(warp, slot, click_range, time_sleep):
 def start_farm():
     while True:
         time.sleep(10)
-    
-        farm("/warp " + ores_warp, ores_slot, 6, 5)
-        farm("/warp " + mobs_warp, mobs_slot, 12, 10)
-        farm('/home', 5, 1, 1)
+        
+        farm('/warp ' + ores_warp, ores_slot, 6, 5)
+        farm('/warp ' + mobs_warp, mobs_slot, 12, 10)
+        farm('/home', '5', 1, 1)
         
         time.sleep(delay)
+        
+start_farm()  
